4a7f909f07fa1059522364417882b423ae90a1c1
Kapitalino <fabianud09@gmail.com>
2026-04-14 15:45:25 -0500

    Task 5: implement submit and show_exam_result views and URLs

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -111,7 +111,7 @@ def enroll(request, course_id):
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
 #def submit(request, course_id):
-    def submit(request, course_id):
+def submit(request, course_id):
         course = get_object_or_404(Course, pk=course_id)
         user = request.user
         enrollment = Enrollment.objects.get(user=user, course=course)
@@ -119,7 +119,7 @@ def enroll(request, course_id):
         choices = extract_answers(request)
         submission.choices.set(choices)
         submission_id = submission.id
-        return HttpResponseRedirect(reverse(viewname='onlinecourse:exam_result', args=(course_id, submission_id,)))
+        return HttpResponseRedirect(reverse('onlinecourse:exam_result', args=(course_id, submission_id,)))
 
 
 # An example method to collect the selected choices from the exam form from the request object
